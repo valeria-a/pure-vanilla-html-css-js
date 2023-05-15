1632b53fae5546f818712ce03db561b5dce54aa5
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-05-15 19:49:29 +0300

    more

--- h2_jb/bored_app.js
+++ h2_jb/bored_app.js
@@ -1,7 +1,28 @@
+
+const BORED_API_BASE_URL = 'http://www.boredapi.com/api/activity'
+
 const handleSubmit = (event) => {
 
     event.preventDefault();
 
-    
+    const formData = new FormData(event.target)
+    const data = Object.fromEntries(formData.entries())
+
+    console.log(data)
+
+    fetch(`${BORED_API_BASE_URL}?type=${data.activity_type}`)
+        .then((response) => {
+            if (response.status !== 200) {
+                throw `Error status: ${response.status}`
+            } else {
+                return response.json()
+            }
+        })
+        .then((jsonData) => {
+            console.log('Received activity:', jsonData)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
 
 }
