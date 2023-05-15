21c4ff98c65e01f7ab36ab3a5e93271af26cb5df
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-05-15 20:06:08 +0300

    more updates

--- h2_jb/bored_app.js
+++ h2_jb/bored_app.js
@@ -1,9 +1,13 @@
 
-const BORED_API_BASE_URL = 'http://www.boredapi.com/api/activity'
+const BORED_API_BASE_URL = 'http://www.boredapi.com/api/activityaaaaaaa'
 
 const handleSubmit = (event) => {
 
     event.preventDefault();
+
+    // disable elements
+    const btn = document.getElementById('submit-btn-id')
+    btn.setAttribute('disabled', true)
 
     const formData = new FormData(event.target)
     const data = Object.fromEntries(formData.entries())
@@ -20,9 +24,18 @@ const handleSubmit = (event) => {
         })
         .then((jsonData) => {
             console.log('Received activity:', jsonData)
+
+            if ('error' in jsonData) {
+                throw `Received error: ${jsonData.error}`
+            }
+
+            document.getElementById('activity').innerText = jsonData.activity
+            btn.removeAttribute('disabled')
         })
         .catch((error) => {
             console.error(error)
+            document.getElementById('activity').innerText = 'Error'
+            btn.removeAttribute('disabled')
         })
 
 }
